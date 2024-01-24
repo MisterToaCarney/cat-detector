f1fdab8c957d5f2f3ea998f109c3ec6c686bbd71
Toa Carney <toa@toacarney.nz>
2024-01-24 18:56:39 +1300

    Increase resolution & save full frames

--- 1.py
+++ 1.py
@@ -1,46 +1,33 @@
+from capture_interface import SecurityCapture
 import cv2
 from ultralytics import YOLO
-
-vcap = cv2.VideoCapture('rtsp://admin:Drumkit1@192.168.1.8/')
-
-model = YOLO('yolov8l.pt')
+import sys
 
 def end():
   vcap.release()
   cv2.destroyAllWindows()
-  exit()
 
-if not vcap.isOpened(): 
-  print("Cannot open camera")
-  exit()
-
+vcap = SecurityCapture(f"rtsp://{sys.argv[1]}@192.168.1.8/", 30)
+model = YOLO('yolov8m.pt')
 frame_count = 0
 
 while True:
-  ret = vcap.grab()
-  if not ret:
-    print("Cant get frame. Exiting...")
-    end()
-
   frame_count += 1
-
-  if (frame_count % 15 == 0): 
-    ret, frame = vcap.retrieve()
-  else:
-    continue
+  ret, frame = vcap.read()
 
   if not ret:
     print("Cant get frame. Exiting...")
-    end()
+    break
 
-  resized = cv2.resize(frame, [640, 480])
+  resized = cv2.resize(frame, None, fx=0.5, fy=0.5)
+  resized = resized[32*4:, :]
 
-  results = model.predict(resized, device='cpu')
+  results = model.predict(resized, device='cpu', imgsz=(resized.shape[0], resized.shape[1]))
   result = results[0]
 
   for index, box in enumerate(result.boxes.xyxy.round().long()):
-    cv2.rectangle(resized, box[0:2].numpy(), box[2:4].numpy(), (0,255,0))
     class_name = result.names[int(result.boxes.cls[index])]
+    cv2.rectangle(resized, box[0:2].numpy(), box[2:4].numpy(), (0,255,0))
     cv2.putText(resized, class_name, box[0:2].numpy(), cv2.QT_FONT_NORMAL, 0.5, (0,255,0), 1, cv2.LINE_AA)
 
   cv2.imshow('frame', resized)
--- image_cap.py
+++ image_cap.py
@@ -2,10 +2,12 @@ from capture_interface import SecurityCapture
 import cv2
 from ultralytics import YOLO
 import os
+import sys
 
 os.makedirs('images/', exist_ok=True)
+os.makedirs('frames/', exist_ok=True)
 
-vcap = SecurityCapture('rtsp://admin:Drumkit1@192.168.1.8/', 10)
+vcap = SecurityCapture(f"rtsp://{sys.argv[1]}@192.168.1.8/", 10)
 model = YOLO('yolov8m.pt')
 
 frame_count = 0
@@ -14,14 +16,21 @@ while True:
   frame_count += 1
   ret, frame = vcap.read()
 
-  resized = cv2.resize(frame, [640, 480])
+  resized = cv2.resize(frame, None, fx=0.5, fy=0.5)
+  resized = resized[32*4, :] # crop
 
-  results = model.predict(resized, device='cpu')
+  results = model.predict(resized, device='cpu', imgsz=(resized.shape[0], resized.shape[1]))
   result = results[0]
 
+  all_classes = []
   for index, box in enumerate(result.boxes.xyxy.round().long()):
     class_name = result.names[int(result.boxes.cls[index])]
-    if class_name in ['car', 'traffic light', 'bus', 'potted plant']: continue
+    if class_name in ['car', 'traffic light', 'bus', 'potted plant', 'tv']: continue
+    all_classes.append(class_name)
     y_s, x_s, y_e, x_e = box.tolist()
     cropped_frame = resized[x_s:x_e,y_s:y_e]
     cv2.imwrite(f"images/{class_name}_{frame_count}_{index}.jpg", cropped_frame)
+  
+  if len(all_classes) > 0:
+    classes_str = "_".join(all_classes)
+    cv2.imwrite(f"frames/{classes_str}-{frame_count}.jpg")
